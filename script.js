fc1a13489cc38a24cdef5e933fc7d5051f7782cd
Olgaif52 <38472080+Olgaif52@users.noreply.github.com>
2018-04-17 20:23:44 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+﻿function dropdownFunc() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onhover = function(e) {
+  if (!e.target.matches('.dropbtn')) {
+    var myDropdown = document.getElementById("myDropdown");
+      if (myDropdown.classList.contains('show')) {
+        myDropdown.classList.remove('show');
+      }
+  }
+}
+
+function clock() {
+var d = new Date();
+var month_num = d.getMonth()
+var day = d.getDate();
+var hours = d.getHours();
+var minutes = d.getMinutes();
+var seconds = d.getSeconds();
+
+month=new Array("січня", "лютого", "березня", "квітня", "травня", "червня",
+"липня", "серпня", "вересня", "жовтня", "листопада", "грудня");
+
+if (day <= 9) day = "0" + day;
+if (hours <= 9) hours = "0" + hours;
+if (minutes <= 9) minutes = "0" + minutes;
+if (seconds <= 9) seconds = "0" + seconds;
+
+date_time = "Сьогодні - " + day + " " + month[month_num] + " " + d.getFullYear() +
+" р.&nbsp;&nbsp;&nbsp;Поточний час - "+ hours + ":" + minutes + ":" + seconds;
+if (document.layers) {
+ document.layers.doc_time.document.write(date_time);
+ document.layers.doc_time.document.close();
+}
+else document.getElementById("doc_time").innerHTML = date_time;
+ setTimeout("clock()", 1000);
+}
+
+function saveVote(){
+	alert("Ваш голос враховано!");
+	document.getElementById("formVote").innerHTML = "";
+}
+
